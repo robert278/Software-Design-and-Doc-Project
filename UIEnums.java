a5f490a68d884ce36be977ca78f1930391770420
ASequera2 <sequea@rpi.edu>
2019-10-09 19:45:05 -0400

    Added Enums for the options of the UI.

---
+++ UIEnums.java
@@ -0,0 +1,17 @@
+public class UIEnums {
+   public enum RequestType {
+      GENERATE, STOP, SAVE;
+   }
+   public enum Key {
+      C, CSHARP, D, DSHARP, E, F, FSHARP, G, GSHARP, A, ASHARP, B;
+   }
+   enum KeySignature {
+      FOURFOUR, TWOTWO, TWOFOUR, THREEFOUR, THREEEIGHT; 
+   }
+   enum Tempo {
+      GRAVE, LARGO, LENTO, ADAGIO, ANDANTE;
+   }
+   enum Emotion {
+      JOY, EXCITEMENT, SURPRISE, SADNESS, DEPRESS, CURE;
+   }
+}
