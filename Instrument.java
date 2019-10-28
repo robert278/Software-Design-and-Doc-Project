bedc63e8bbb1ef4e03de93159ad5b9c7ca45941c
Priyanshu Tripathi <priyanshutripathi@shazam-221.dynamic2.rpi.edu>
2019-10-28 16:43:24 -0400

    Set of default 10 instruments

---
+++ Instrument.java
@@ -0,0 +1,15 @@
+package mymusic;
+
+public class Instrument {
+	private String type;
+	public Instrument(String type) {
+		this.setType(type);
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
